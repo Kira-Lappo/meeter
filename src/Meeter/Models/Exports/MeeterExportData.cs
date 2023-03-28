e5e5e379d2efa6591ca838d677f202f69290fefd
Kirill Lappo <kirill-lappo@outlook.com>
2023-03-28 14:15:30 +0300

    added project with base models

---
+++ src/Meeter/Models/Exports/MeeterExportData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meeter.Models.Exports;
+
+public class MeeterExportData
+{
+    public DateTime ExportDateTime { get; set; }
+
+    public List<Meeting> Items { get; set; }
+}
---
+++ src/Meeter/Models/Meeting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Meeter.Models;
+
+public class Meeting
+{
+    public string Subject { get; set; }
+
+    public DateTime StartDateTime { get; set; }
+
+    public DateTime EndDateTime { get; set; }
+
+    public TimeSpan NotifyBeforeTime { get; set; }
+}
