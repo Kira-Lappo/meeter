06e01d2332c0e0608e5cb82b72c57880344f213e
Kirill Lappo <kirill-lappo@outlook.com>
2023-03-29 13:09:15 +0300

    fixed publish script

--- publish.ps1
+++ publish.ps1
@@ -1,12 +1,13 @@
 #!/bin/pwsh
+
 param(
     [Parameter()]
     [ValidateSet("linux-x64", "win-x64")]
     $RID = "win-x64"
 )
 
-dotnet publish $PSScriptRoot/src/Meeter.Cli `
+dotnet publish "$PSScriptRoot/src/Meeter.Cli" `
     -c Release  `
-    -p:PublishProfile = "publish.pubxml"  `
-    -p:RuntimeIdentifier = "$RID"  `
+    -p:PublishProfile="publish.pubxml"  `
+    -p:RuntimeIdentifier="$RID"  `
     -o "$PSScriptRoot/out/$RID"
